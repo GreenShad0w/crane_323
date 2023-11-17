5df3d8c472cc6cc5a7d963bb72edb3d49a198456
SkiprDAG <SkiprDAG@yandex.ru>
2023-11-17 15:08:27 +0800

    fix

--- globalgamemanagers/Assets/Scripts/Assembly-CSharp/MoveCrane.cs
+++ globalgamemanagers/Assets/Scripts/Assembly-CSharp/MoveCrane.cs
@@ -23,20 +23,20 @@ public class MoveCrane : MonoBehaviour
 			try
 			{
 				int x = spCrane.ReadByte();
-				if (x == 1)
+				if (x == 2)
 				{
 					if (this.obj_UD.transform.position.y < 0)
 					{
-						this.obj_UD.transform.Translate(-Vector3.up * speed);
+						this.obj_UD.transform.Translate(-Vector3.up * speed); //Крюк вверх
 						//Debug.Log(this.obj_UD.transform.position.y);
 					}
 					this.Btn6.transform.position = Vector3.Lerp(StartPositionBtn6, EndPositionBtn6, step);
 				}
-				if (x == 2)
+				if (x == 6)
 				{
 					if (this.obj_UD.transform.position.y > -7.5)
 					{
-						this.obj_UD.transform.Translate(Vector3.up * speed);
+						this.obj_UD.transform.Translate(Vector3.up * speed); //Крюк вниз
 						//Debug.Log(this.obj_UD.transform.position.y);
 					}
 					this.Btn5.transform.position = Vector3.Lerp(StartPositionBtn5, EndPositionBtn5, step);
@@ -45,32 +45,34 @@ public class MoveCrane : MonoBehaviour
 				{
 					if (transform.position.z > -25)
 					{
-						transform.Translate(Vector3.forward * speed);
+						transform.Translate(Vector3.forward * speed); //Кран влево
+						//Debug.Log(transform.position.z);
 					}
 					this.Btn4.transform.position = Vector3.Lerp(StartPositionBtn4, EndPositionBtn4, step);
 				}
-				if (x == 4)
+				if (x == 5)
 				{
 					if (transform.position.z < 44)
 					{
-						transform.Translate(-Vector3.forward * speed);
+						transform.Translate(-Vector3.forward * speed); //Кран вправо
+						//Debug.Log(transform.position.z);
 					}
 					this.Btn3.transform.position = Vector3.Lerp(StartPositionBtn3, EndPositionBtn3, step);
 				}
-				if (x == 5)
+				if (x == 4)
 				{
 					if (this.obj_FB.transform.position.x < 3)
 					{
-						this.obj_FB.transform.Translate(Vector3.forward * speed);
+						this.obj_FB.transform.Translate(Vector3.forward * speed); //Кабина назад
 					}
 					this.Btn2.transform.position = Vector3.Lerp(StartPositionBtn2, EndPositionBtn2, step);
 				}
-				if (x == 6)
+				if (x == 1)
 				{
 					if (this.obj_FB.transform.position.x > -34)
 					{
-						this.obj_FB.transform.Translate(-Vector3.forward * speed);
-						//Debug.Log(this.obj_FB.transform.position.x);
+						this.obj_FB.transform.Translate(-Vector3.forward * speed); //Кабина вперед
+						Debug.Log(this.obj_FB.transform.position.x);
 					}
 					this.Btn1.transform.position = Vector3.Lerp(StartPositionBtn1, EndPositionBtn1, step);
 				}
